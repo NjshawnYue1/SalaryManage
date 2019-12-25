2a75d9b048c11dfa89035753a6fb9e3fd5f065f0
GuAn <njguan1995@gmail.com>
2019-12-25 13:23:01 +0800

    添加部分接口参数swagger注释

--- src/main/java/com/usts/salarymanage/controller/EmployeeManageController.java
+++ src/main/java/com/usts/salarymanage/controller/EmployeeManageController.java
@@ -12,6 +12,7 @@ import com.usts.salarymanage.model.Salary;
 import com.usts.salarymanage.service.EmployeeService;
 import com.usts.salarymanage.service.SalaryService;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
@@ -66,9 +67,9 @@ public class EmployeeManageController {
         return AjaxResponse.success(employeeVo);
     }
 
-    @ApiOperation("查询所有员工信息")
+    @ApiOperation("查询所有员工信息 返回分页查询的结果")
     @GetMapping("/employees")
-    public AjaxResponse queryAll(@RequestBody Page page) {
+    public AjaxResponse queryAll(@ApiParam("分页参数封装 页数和每页数量") @RequestBody Page page) {
         PageHelper.startPage(page.getPageNum(), page.getPageSize());
         List<Employee> employeeList = tblEmployeeDao.selectByExample(null);
         List<EmployeeVo> employeeVoList = new ArrayList<>();
@@ -95,7 +96,7 @@ public class EmployeeManageController {
 
     @ApiOperation("根据id更新员工信息 返回操作是否成功")
     @PutMapping("/employees/{id}")
-    public AjaxResponse updateById(@PathVariable int id, @RequestBody EmployeeVo employeeVo) {
+    public AjaxResponse updateById(@PathVariable  int id, @RequestBody EmployeeVo employeeVo) {
         Employee employee = new Employee();
         BeanUtil.copyProperties(employeeVo, employee);
         employee.setId(id);
@@ -110,10 +111,10 @@ public class EmployeeManageController {
         return AjaxResponse.success(flag);
     }
 
-    @ApiOperation("根据职位分页查询员工,返回员工列表")
+    @ApiOperation("根据职位id分页查询员工,返回员工列表")
     @GetMapping("/employees/careers/{careerId}")
-    public AjaxResponse selectByCareerId(@PathVariable int careerId,@RequestBody Page page) {
-        PageHelper.startPage(page.getPageNum(),page.getPageSize());
+    public AjaxResponse selectByCareerId(@PathVariable int careerId, @RequestBody Page page) {
+        PageHelper.startPage(page.getPageNum(), page.getPageSize());
         List<Employee> employeeList = employeeService.queryByCareerId(careerId);
         List<EmployeeVo> employeeVoList = new ArrayList<>();
         Iterator<Employee> iterator = employeeList.iterator();
@@ -136,10 +137,10 @@ public class EmployeeManageController {
         return AjaxResponse.success(pageInfo);
     }
 
-    @ApiOperation("根据部门分页查询员工列表")
+    @ApiOperation("根据部门id分页查询员工列表")
     @GetMapping("/employees/departments/{departmentId}")
-    public AjaxResponse selectByDepartmentId(@PathVariable int departmentId,@RequestBody Page page) {
-        PageHelper.startPage(page.getPageNum(),page.getPageSize());
+    public AjaxResponse selectByDepartmentId(@PathVariable int departmentId, @RequestBody Page page) {
+        PageHelper.startPage(page.getPageNum(), page.getPageSize());
         List<Employee> employeeList = employeeService.queryByCareerId(departmentId);
         List<EmployeeVo> employeeVoList = new ArrayList<>();
         Iterator<Employee> iterator = employeeList.iterator();
